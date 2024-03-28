fe0362188f9ac9ad6a3c0057face3aae0e338814
GianmarcoBramucci <giangian797@gmail.com>
2024-03-28 17:00:29 +0100

    add script ese(no bonus)

--- js/script.js
+++ js/script.js
@@ -4,14 +4,33 @@ Vue.createApp({
     data(){
         return{
             slides,
-            active: {
-                flag: true
-                
-            }
-            
+            ativeIndex : 0
         }
     },
     methods: {
-
+        ative: function(i){
+            if(i === this.ativeIndex){
+                return 'active thumb';
+            }
+            else{
+                return 'thumb';
+            }
+        },
+        slideForward: function(){
+            if(this.ativeIndex<slides.length-1){
+                this.ativeIndex++;
+            }
+            else{
+                this.ativeIndex=0;
+            }
+        },
+        slideBack: function(){
+            if(this.ativeIndex>0){
+                this.ativeIndex--;
+            }
+            else{
+                this.ativeIndex=slides.length-1;
+            }
+        }
     }
 }).mount('#app')
